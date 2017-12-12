e44900f8ad4b36bd293a5444e442abd2f2df0f6d
Sanghak,Lee <code.ryan.lee@gmail.com>
2017-12-12 14:26:51 +0900

    github 사용자 정보 가져오기

--- 2017/12.12/python/lsh-get-github-user.py
+++ 2017/12.12/python/lsh-get-github-user.py
@@ -1,2 +1,19 @@
-def getUser():
-    return "Sanghak,Lee / http://sanghaklee.tistory.com" #FIXME:
+import requests
+
+def getUser(username):
+    res = requests.get('https://api.github.com/users/{0}'.format(username))
+    body = res.json()
+
+    if body['name'] is not None:
+        name = body['name']
+    else:
+        name = body['login']
+    
+    if body['blog'] != '':
+        blog = body['blog']
+    else:
+        blog = body['html_url']
+        
+    return '{0} / {1}'.format(name, blog)
+
+print(getUser('jya9055'))
