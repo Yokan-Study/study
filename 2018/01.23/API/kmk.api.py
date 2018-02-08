b7bbc9ea04e27a4015f9666df54cd75c8c70111d
mksweetlife <kmkgabia@gmail.com>
2018-02-08 17:28:29 +0900

    fdasdf
    safdsafsaf

--- 2018/01.23/API/kmk.api.py
+++ 2018/01.23/API/kmk.api.py
@@ -1,15 +1,16 @@
 import requests
 
-r = requests.get('https://api.coinone.co.kr/ticker?currency=all')
+r = requests.get('https://api.coinone.co.kr/ticker?currency={}.format(type)')
 j = r.json()
 
 coin = j["currency"]
 last = j["last"]
 
-def qtum_last_price():
-    if coin == "qtum":
-        print("퀀텀 최종 시세: ", last)
+coin_modu = [btc, bch, eth, etc, xrp, qtum, iota, ltc, btg, all]
 
-    else:
+def getLastPrice(coin):
+    if coin not in coin_modu: 
         return false
 
+    else: 
+        print ("최종 가격: ", last)
--- 2018/01.23/API/lsh.api.py
+++
@@ -1,2 +0,0 @@
-def getPrice():
-    return 0
