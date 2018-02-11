4b3eabf30b9f462652be283d43145d3b60c8e6a0
mksweetlife <kmkgabia@gmail.com>
2018-02-11 16:35:02 +0900

    kmk.members.py

--- 2018/02.07/kmk.members.py
+++ 2018/02.07/kmk.members.py
@@ -1,5 +1,5 @@
 import requests
-    headers = {'Authorization': 'Basic {KEY}'} #{KEY}값이랑 아래 headers 값은 mock 처리해야
+    headers = {'Authorization': 'Basic {KEY}'} #{KEY}값이랑 아래 headers 값은 mock 처리하랬는데 어떻게?
 
     def getMember(user_id): #함수로 회원 정보 가져오기 
     r = requests.get('https://gapi.gabia.com/members?user_id.{0}'.format(user_id), headers = headers) #포맷함수 지리구영 맞나?ㅎㅎ 
