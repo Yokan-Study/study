22e24e802302558672f932a4b7fc92eea967241d
mksweetlife <kmkgabia@gmail.com>
2018-03-18 14:54:14 +0900

    kmk.members.py
    commit kmk.members.py

--- 2018/02.07/kmk.members.py
+++ 2018/02.07/kmk.members.py
@@ -1,14 +1,38 @@
 import requests
-    headers = {'Authorization': 'Basic {KEY}'} #{KEY}값이랑 아래 headers 값은 mock 처리하랬는데 어떻게?
+headers = {'Authorization' : 'Basic {KEY}'}
 
-    def getMember(user_id): #함수로 회원 정보 가져오기 
-    r = requests.get('https://gapi.gabia.com/members?user_id.{0}'.format(user_id), headers = headers) #포맷함수 지리구영 맞나?ㅎㅎ 
-    return r.json('hanadmin') #json으로 리턴된 회원 정보 중에서 한글이름 가져오기 
+#1명의 회원 이름 정보 가져오는 함수 getMember() 만들기
+#return 한글이름 hanadmin(string)
 
-    def getMembers(user_ids): #함수로 여러 회원 정보 가져오기
-        user_ids = getMember(user_id)
-        for x in user_ids:
-        return x
+def getMember(user_id):
+    r = requests.get('https://gapi.gabia.com/members?user_id={0}'.format(user_id), headers=headers)
+    #print(r)
+    j = r.json()
+    name = j['client_info']['hanadmin']
+    
+    return name 
 
-        
+r = 'kmkyeongkim11'
+# print getMember(r)
+
+k = getMember(r)
+print(k)
+
+def getMembers(user_ids):
+    members = []
+    for id in user ids:
+        c = getMember(id)
+        members.append(c)
+
+    return members
+
+kmk1 = ['kmkyeongkim11', 'test1gabia', 'test0gabia']
+kmk2 = getMembers(kmk1)
+
+print(kmk2)
+
+
+
+
+
 
