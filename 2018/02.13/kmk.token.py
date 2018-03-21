af8ce5bf05b51683bf7fe93d7d9a89e594908914
Minkyeong <kmkgabia@gmail.com>
2018-03-21 19:17:18 +0900

    kmk.token.py

--- 2018/02.13/kmk.token.py
+++ 2018/02.13/kmk.token.py
@@ -5,15 +5,21 @@ def getToken():
     r = requests.post('https://gapi.gabia.com/oauth/token', data = {'client_id':'www_front', 'client_secret':'eoqhfma', 'grant_type':'client_credentials'})
     j = r.json()
     token = j['access_token']
-    return token
+    return 'www_front:{0}'.format(token)
 
 p = getToken()
 print(p)
 
 
 def makeHeadersAuth(token):
-    token_encoded = base64.b64encode(token)
-    # 모르겠다아아~~~~~
-    # 힌트 좀 주세요~~~~~
-    return auth
+    fucking_bytes = bytes(token, 'utf-8')
+    token_encoded = base64.b64encode(fucking_bytes)
+    Rjwu_bytes = token_encoded.decode('utf-8')
+    #print(Rjwu_bytes)
+
+    return Rjwu_bytes
+
+def hanbang():
+    return makeHeadersAuth(getToken())
+#print(hanbang())
  
