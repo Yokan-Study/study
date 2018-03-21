68dd6d151b987f0d84996703e317d41caa6f8352
Minkyeong <kmkgabia@gmail.com>
2018-03-21 19:44:08 +0900

    hi yum

--- 2018/02.13/kmk.token.py
+++ 2018/02.13/kmk.token.py
@@ -1,8 +1,11 @@
 import requests
 import base64
+import config
+
+#print(config.api_config)
 
 def getToken():
-    r = requests.post('https://gapi.gabia.com/oauth/token', data = {'client_id':'www_front', 'client_secret':'eoqhfma', 'grant_type':'client_credentials'})
+    r = requests.post('https://gapi.gabia.com/oauth/token', data = config.api_config)
     j = r.json()
     token = j['access_token']
     return 'www_front:{0}'.format(token)
