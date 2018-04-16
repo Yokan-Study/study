44ce9ff2fd188d670b3cda6b68424ab912cf393b
mksweetlife <kmkgabia@gmail.com>
2018-04-16 15:43:02 +0900

    commit kmk.class.py

--- 2018/04.11/kmk.class.py
+++ 2018/04.11/kmk.class.py
@@ -1,5 +1,4 @@
 # gapi 클래스 만들기
-
 import requests
 import base64
 import config
@@ -7,8 +6,8 @@ import config
 class gapiClass:
 
 # step 1. 인증 받기 위한 함수를 만든다. 
-    def getToken(self,host = ):
-        r = requests.post('https://gapi.gabia.com/oauth/token', data = config.api_config)
+    def getToken(self, host='https://gapi.gabia.com'):
+        r = requests.post('{0}/oauth/token'.format(host), data = config.api_config)
         j = r.json()
         token = j['access_token']
         return token
@@ -16,14 +15,16 @@ class gapiClass:
 p = getToken()
 #print(p)
 
-
 # step 2. step 1에서 받은 토큰 값을 이용해 header에 넣을 인증키를 생성하는 함수를 만든다. 
-    def makeHeadersAuth(token): 
+    def makeHeadersAuth(self, token): 
         fucking_bytes = bytes(token, 'utf-8')
         token_encoded = base64.b64encode(fucking_bytes)
         Rjwu_bytes = token_encoded.decode('utf-8')
         return Rjwu_bytes
 
 # step 3. 토큰 받고 인증키 입력하는 것을 한방에 해결하는 함수를 만든다. 
-    def hanbang():
+    def hanbang(self):
         return makeHeadersAuth(getToken())
+
+
+# step 4. 
