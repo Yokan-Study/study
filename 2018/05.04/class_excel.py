662bf9e76b57d5cdc14c993308c4370f30bdac32
mksweetlife <kmkgabia@gmail.com>
2018-05-04 17:59:53 +0900

    일단 모듈로 클래스 함수 불러오는 것까지 했어요..

---
+++ 2018/05.04/class_excel.py
@@ -0,0 +1,10 @@
+#1. 클래스 파일 class_kmk.py 모듈로 불러오기 (참고 사이트: https://wikidocs.net/29)
+
+import class_kmk
+a = class_kmk.gapiClass()
+
+#print(a.getMember('kmkyeongkim11')) 
+
+#2. user id에 따른 hanname 값들을 리스트 형태로 리턴하기 
+#3. 엑셀 파일 및 시트 활성화
+#4. 데이터 입력 (여긴 잘 모르게땅 아직 ㅎㅎ)
