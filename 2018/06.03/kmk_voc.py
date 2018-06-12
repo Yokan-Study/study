3f0a9a6b3ca6477562dc3649d7c3ec1705a5f6c0
Minkyeong <kmkgabia@gmail.com>
2018-06-12 19:49:22 +0900

    kmk_voc.py

--- 2018/06.03/kmk_voc.py
+++ 2018/06.03/kmk_voc.py
@@ -1,5 +1,40 @@
+#-*- coding: utf-8 -*-
 import urllib.request
+import xmltodict
+import json
 with urllib.request.urlopen("http://admin2.gabia.com/voc/improve/getData/service_imp_list_data.php") as url:
 
     r = url.read()
-print(r)
+
+h = r.decode("euc-kr")
+g = xmltodict.parse(h)
+#print(h)
+#print(g)
+j = json.loads(json.dumps(g))
+#print(j)
+
+id = j['rows']['row']
+for i in id:
+    k = i['@id']
+    #print(k)
+
+worker_list = []
+row = j['rows']['row']
+for w in row:
+    o = w['cell'][7]
+    #print(o)
+
+    if o == '최선애':
+        k = w['userdata']['#text']
+        #print(k)
+        worker_list.append(k)
+print(worker_list)
+
+        
+
+
+
+
+
+
+
