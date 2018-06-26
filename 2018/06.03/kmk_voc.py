c59c35ef8df58e33062e107932e5dfc79d199179
mksweetlife <kmkgabia@gmail.com>
2018-06-26 19:59:22 +0900

    게시판 글번소 2개로 딕셔너리 생성

--- 2018/06.03/kmk_voc.py
+++ 2018/06.03/kmk_voc.py
@@ -5,7 +5,7 @@ import json
 with urllib.request.urlopen("http://admin2.gabia.com/voc/improve/getData/service_imp_list_data.php") as url:
 
     r = url.read()
-
+    
 h = r.decode("euc-kr")
 g = xmltodict.parse(h)
 #print(h)
@@ -13,26 +13,31 @@ g = xmltodict.parse(h)
 j = json.loads(json.dumps(g))
 #print(j)
 
-id = j['rows']['row']
-for i in id:
-    k = i['@id']
-    #print(k)
-
-worker_list = []
+#특정 작업자에 해당하는 board_seqno와 seqno 추출하기
+board_seqno = []
+seqno = []
 row = j['rows']['row']
 for w in row:
     o = w['cell'][7]
+    p = w['@id']
     #print(o)
 
     if o == '최선애':
-        k = w['userdata']['#text']
+        k = w['userdata']['#text']  
         #print(k)
-        worker_list.append(k)
-print(worker_list)
+        q = w['@id']
+        #print(q)
+        board_seqno.append(k)
+        seqno.append(q)
 
-        
+#print(board_seqno)
+#print(seqno)
 
-
+#게시판 글 번호 2개 리스트를 딕셔너리로 만들기 
+num_dic = {
+    board_seqno: seqno for board_seqno, seqno in zip(board_seqno, seqno)
+}
+print(num_dic)
 
 
 
